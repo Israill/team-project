fa5357bb1a508b4cdc8814aa4db29eede7853052
Morz <morz@Morzs-MacBook-Pro.local>
2022-06-03 16:08:11 +0300

    добавил контроллеры и роутеры отзывов

--- controllers/review.controller.js
+++ controllers/review.controller.js
@@ -2,5 +2,27 @@ const Review = require("../models/Review.model");
 
 
 module.exports.reviewController = {
-
+    getReviews: async (req, res) => {
+        try {
+            const getRew = await Review.find({})
+            res.json(getRew);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+    postReviews: async (req, res) => {
+        try {
+            const postRew = await Review.create({product: req.body.product, user: req.body.user})
+            res.json(postRew);
+        } catch (e) {
+            res.json(e);
+        }
+    },
+    deleteReviews: async (req, res) => {
+        try {
+            Review.findByIdAndRemove(req.params.id);
+        } catch (e) {
+            res.json(e);
+        }
+    }
 };
--- routes/review.route.js
+++ routes/review.route.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
+const { reviewController } = require("../controllers/review.controller");
 const router = Router();
 
-router.post("/review", );
-
+router.get("/review", reviewController.getReviews);
+router.post("/review", reviewController.postReviews);
+router.delete("review", reviewController.deleteReviews);
 
 module.exports = router;
